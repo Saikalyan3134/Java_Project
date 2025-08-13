49967f3e2cc6eefb43ff8dd9a63d96b8c31a9eb3
Saikalyan3134 <saikalyanmanjunath@gmail.com>
2025-08-13 12:15:34 +0530

    Second Commit

--- src/FroVowels.java
+++ src/FroVowels.java
@@ -1,3 +1,22 @@
-
+import java.util.Scanner;
 public class FroVowels {
+    static String string_changer(String s) {
+        String vow = "", con = "";
+        for (int i = 0; i < s.length(); i++) {
+            char ch=s.charAt(i);
+            char s1=Character.toLowerCase(ch);
+            if (s1 =='a' ||s1 =='e' ||s1 =='i' ||s1 =='o' ||s1 =='u') {
+                vow += ch;
+            } else {
+                con += ch;
+            }
+        }
+        return (vow+con);
+    }
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter a String: ");
+        String s=sc.nextLine();
+        System.out.println("Modified String: "+string_changer(s));
+    }
 }
